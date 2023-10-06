17f41964efae821d93f2f63a3484ee01541cc3b7
Diaa <diaa@gmail.com>
2023-10-06 16:20:20 +0200

    add kyu 6 & 8

---
+++ Javascript/kyu 6/queueTime.js
@@ -0,0 +1,48 @@
+/* There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+input:
+customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+n: a positive integer, the number of checkout tills.
+output:
+The function should return an integer, the total time required. */
+/* Important:
+Please look at the examples and clarifications below, to ensure you understand the task correctly :) */
+/* Examples:
+queueTime([5,3,4], 1)
+// should return 12
+// because when there is 1 till, the total time is just the sum of the times
+queueTime([10,2,3,3], 2)
+// should return 10
+// because here n=2 and the 2nd, 3rd, and 4th people in the 
+// queue finish before the 1st person has finished.
+queueTime([2,3,10], 2)
+// should return 12 */
+/* Clarifications
+There is only ONE queue serving many tills, and
+The order of the queue NEVER changes, and
+The front person in the queue (i.e. the first element in the array/list) proceeds to a till as soon as it becomes free.
+N.B. You should assume that all the test input will be valid, as specified above.
+P.S. The situation in this kata can be likened to the more-computer-science-related idea of a thread pool, with relation to running multiple processes at the same time: https://en.wikipedia.org/wiki/Thread_pool */
+
+// we have an array of the time (number) of each customer take in the till
+// we have a number of the tills
+// we need to return the time (number) takes to finish the queue (customers)
+
+// create new array of the number of tills
+// loop of the customers array
+// take the index of the new array minumum integer and add time to it
+// return the maximum integer of the new array
+
+function queueTime(customers, n) {
+    let newArr = new Array(n).fill(0);
+    for (const time of customers) {
+        let index = newArr.indexOf(Math.min(...newArr))
+        newArr[index] += time;
+    }
+    return Math.max(...newArr)
+}
+
+console.log(queueTime([1,2,3], 1));
+console.log(queueTime([1,2,3,4,5], 100));
+console.log(queueTime([10,3,3,2], 2));
+console.log(queueTime([3,10,3,2], 2));
+console.log(queueTime([2,2,3,3,4,4], 2));
